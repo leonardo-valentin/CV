2f8fe236c6e92df7320dcdd5ba2b336bebda88f9
Leonardo Valentin dos Santos <71048056+leonardo-valentin@users.noreply.github.com>
2023-03-29 20:22:53 -0300

    Add files via upload
    Novo cv

---
+++ teste.js
@@ -0,0 +1,31 @@
+// nome original app.js
+let textoquebralinhafoda = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa"
+
+pog = ["oi", "ok"]
+
+for (let i = 0; i < pog.length; i++) {
+    const element = pog[i];
+}
+
+
+
+//shift alt f     (corrigir codigo)        ///  alt setinha   (alterar linha do cod)       // ctrl shift k   (apagar uma linha toda)
+
+// 
+
+
+//19 min
+
+
+
+//git init
+
+//git status
+
+//git remote add origin https://trallaa
+
+//git add .
+
+//git commit -m "message"
+
+//git push origin master
